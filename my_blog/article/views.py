079a3d1fb36bf320df04646aca453dd788da4ad6
lj365146534 <30396073+lj365146534@users.noreply.github.com>
2017-11-23 15:00:47 +0800

    Add files via upload

---
+++ my_blog/article/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from article.models import Article
+from datetime import datetime
+from django import template
+
+# Create your views here.
+
+
+def home(request):
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
+
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post' : post})
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+    
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
